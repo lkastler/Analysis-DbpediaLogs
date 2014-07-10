d3c589282c8bf72c3bd4f3737d778cb6d3291c61
Leon Kastler <lkastler@uni-koblenz.de>
2014-07-10 12:09:42 +0200

    added some exception handling and comments

--- DatasetAnalyzer/src/SparqlExtraction.py
+++ DatasetAnalyzer/src/SparqlExtraction.py
@@ -6,27 +6,26 @@ Created on Jun 25, 2014
 
 if __name__ == '__main__':
 	import Config as config
+	from dbpedia_analyzer import Extractor as ex
+	from dbpedia_analyzer import FileParser as fp
 	import logging
 	logging.basicConfig(filename=config.extractionLog,level=logging.DEBUG)
 	
-	logger = logging.getLogger(__name__)
-	
+	logger = logging.getLogger("main")
 	malsparql = open(config.malformedsparql, 'w+')
 	sparqls = open(config.sparqloutput, 'w+')
-	
-	from dbpedia_analyzer import FileParser as fp	
-	from dbpedia_analyzer import Extractor as ex
-	
 	extract = ex.Extractor(logger, sparqls, malsparql)
-	
-	for f in fp.parseFolder(logger, config.inputfolder):
-		for item in fp.parseFile(logger, f):
-			extract.extract(item)
-	
-	sparqls.flush()
-	sparqls.close()
-	
-	malsparql.flush()
-	malsparql.close()
-	
-	logger.info(extract.stats)
+	try:
+		for f in fp.parseFolder(logger, config.inputfolder):
+			for item in fp.parseFile(logger, f):
+				extract.extract(item)
+	except Exception as ex:
+		logger.error("Exception: ", ex)
+	finally:
+		sparqls.flush()
+		sparqls.close()
+		
+		malsparql.flush()
+		malsparql.close()
+		
+		logger.info(extract.stats)
--- DatasetAnalyzer/src/dbpedia_analyzer/Extractor.py
+++ DatasetAnalyzer/src/dbpedia_analyzer/Extractor.py
@@ -4,7 +4,7 @@ Created on Jul 2, 2014
 @author: lkastler
 '''
 class Extractor:
-	''''''
+	''' extracts urls and sparql queries from dbpedia server log files and tracks information about the data.'''
 	def __init__(self, logger, sparqls, malformed):
 		import re
 		self.logger = logger
@@ -25,41 +25,47 @@ class Extractor:
 		self.malformed = malformed
 		self.logger.info("created extractor")
 	
-	''''''
+	''' starts the extraction process for one line encoding a Apache log file entry '''
 	def extract(self, line):
 		self.stats['count'] += 1
-		self.analyzeUrl(self.extractUrl(line))
+		
+		self.extractUrl(line)
 	
-	''''''
+	''' extracts a url from a apache log file '''
 	def extractUrl(self, line):
-		request = line.split('"')[1]
-		request = request.split(' ')[1]
-		return request
+		try:
+			request = line.split('"')[1]
+			request = request.split(' ')[1]
+			
+			self.analyzeUrl(request)
+		except Exception as error:
+			self.logger.info(str([str(type(error)), str(error)]))
 	
-		''''''
+	''' tests regex pattern on the given url snipplet and executes corresponding functions if they match '''
 	def analyzeUrl(self, url):
 		for pattern, func in self.patterns.iteritems():
 			if pattern.match(url) != None:
 				func(url)
 	
-	''''''
+	''' extracts sparql query from the given url snipplet '''
 	def extractSparql(self, url): 
 		import urllib2
 		import re
 		
 		request = re.sub("/sparql\?", '', url)
-		request = request.split('&')[0]
+		req_split = request.split('&')
 		
-		if re.match('^query', request) != None:
-			request = re.sub('^query=', '', request)
-			request = urllib2.unquote(request)
-			request = re.sub('(\+)+', ' ', request)
-			request = re.sub('\s+', ' ', request)
-			request = request.strip()
-			
-			self.validateSparql(request)
+		for param in req_split:
+			if re.match('^query', param) != None:
+				query = re.sub('^query=', '', param)
+				query = urllib2.unquote(query)
+				query = re.sub('(\+)+', ' ', query)
+				query = re.sub('\s+', ' ', query)
+				query = query.strip()
+				
+				self.validateSparql(query)
 	
-	''''''
+	''' validates a sparql query and stores it in a separate file '''
 	def validateSparql(self, sparql):
 		import rdflib.plugins.sparql as validator
 		
@@ -74,9 +80,11 @@ class Extractor:
 			self.stats['malformed-sparql'] += 1 
 			self.malformed.write(str([str(type(error)), str(error), sparql]) + '\n')
 	
+	''' analyzes information about the resource request'''
 	def handleResource(self, url):
 		self.stats["resource"] += 1
 	
+	''' analyzes information about the ontology request '''
 	def handleOntology(self, url):
 		self.stats['ontology'] += 1
 	
