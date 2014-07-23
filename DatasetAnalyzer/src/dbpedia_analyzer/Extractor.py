59f03d2cfb3297c88d3ae4ca57a76f6f49479067
Leon Kastler <lkastler@uni-koblenz.de>
2014-07-23 10:26:54 +0200

    polished extractor, needs still testing

--- DatasetAnalyzer/src/dbpedia_analyzer/Extractor.py
+++ DatasetAnalyzer/src/dbpedia_analyzer/Extractor.py
@@ -3,56 +3,73 @@ Created on Jul 2, 2014
 
 @author: lkastler
 '''
+
+import apachelog
+import rdflib.plugins.sparql as validator
+import re
+import urllib2
+from collections import defaultdict
+		
 class Extractor:
 	''' extracts urls and sparql queries from dbpedia server log files and tracks information about the data.'''
+	
+	'''general purpose apache log file structure'''
+	_log_format = r'%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+	
+	'''identifies the primary resource'''
+	_primary_resource = re.compile('^/\w+(\?|/)?')
+	
 	def __init__(self, logger, sparqls, malformed):
-		import re
-		self.logger = logger
+		'''initial set up'''
+		# logging
+		self.log = logger
 		
-		self.stats = {}
-		self.stats['count'] = 0
-		self.stats['resource'] = 0
-		self.stats['ontology'] = 0
-		self.stats['malformed-sparql'] = 0
-		self.stats['sparql'] = 0
+		# stats
+		self.stats = defaultdict(int)
 		
-		self.patterns = dict()
-		self.patterns[re.compile('^/sparql')] = self.extractSparql
-		self.patterns[re.compile('^/(resource|page)')] = self.handleResource
-		self.patterns[re.compile ('^/ontology')] = self.handleOntology
-		
+		# files
 		self.sparqls = sparqls
 		self.malformed = malformed
-		self.logger.info("created extractor")
+		
+		# apache log file parser
+		self.parser = apachelog.parser(Extractor._log_format)
+		
+		self.log.info("created extractor")
 	
-	''' starts the extraction process for one line encoding a Apache log file entry '''
+	
 	def extract(self, line):
+		''' starts the extraction process for one line encoding a Apache log file entry '''
 		self.stats['count'] += 1
 		
-		self.extractUrl(line)
+		self.extractResource(line)
 	
-	''' extracts a url from a apache log file '''
-	def extractUrl(self, line):
-		try:
-			request = line.split('"')[1]
-			request = request.split(' ')[1]
-			
-			self.analyzeUrl(request)
-		except Exception as error:
-			self.logger.info(str([str(type(error)), str(error)]))
-	
-	''' tests regex pattern on the given url snipplet and executes corresponding functions if they match '''
-	def analyzeUrl(self, url):
-		for pattern, func in self.patterns.iteritems():
-			if pattern.match(url) != None:
-				func(url)
-	
-	''' extracts sparql query from the given url snipplet '''
-	def extractSparql(self, url): 
-		import urllib2
-		import re
+	def extractResource(self, line):
+		''' extracts the resource from a apache log file '''
+		res = self.parser.parse(line)
+		self.analyzeResource(res['%r'])
 		
-		request = re.sub("/sparql\?", '', url)
+	
+	
+	def analyzeResource(self, res):
+		''' tests regex pattern on the given url snipplet and executes corresponding functions if they match '''
+		primRes = Extractor._primary_resource.match(res)
+		
+		if  primRes != None:
+			identifier = re.sub('(\?|/)', '', primRes.group(0))
+			
+			if identifier != None:
+				self.stats[identifier] += 1
+				
+				# handle sparql differentyl
+				if identifier == "sparql":
+					self.extractSparql(res)
+		else:
+			self.log.warn("could not identify kind of request", res)
+	
+	
+	def extractSparql(self, res):
+		''' extracts sparql query from the given resource snipplet ''' 
+		request = re.sub("/sparql\?", '', res)
 		req_split = request.split('&')
 		
 		for param in req_split:
@@ -65,27 +82,14 @@ class Extractor:
 				
 				self.validateSparql(query)
 	
-	''' validates a sparql query and stores it in a separate file '''
+	
 	def validateSparql(self, sparql):
-		import rdflib.plugins.sparql as validator
-		
+		''' validates a sparql query and stores it in a separate file '''
 		sparql = str(sparql)
 		
 		try:
 			validator.prepareQuery(sparql)
-			self.stats['sparql'] += 1
 			self.sparqls.write(sparql + '\n')
-			
 		except Exception as error:
 			self.stats['malformed-sparql'] += 1 
 			self.malformed.write(str([str(type(error)), str(error), sparql]) + '\n')
-	
-	''' analyzes information about the resource request'''
-	def handleResource(self, url):
-		self.stats["resource"] += 1
-	
-	''' analyzes information about the ontology request '''
-	def handleOntology(self, url):
-		self.stats['ontology'] += 1
-	
-	
